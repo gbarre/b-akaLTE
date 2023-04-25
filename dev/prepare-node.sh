a0475ee66200a7aa4133c497f471e86430d3266c
Guillaume Barré <guillaume.barre@ac-versailles.fr>
2023-04-25 14:23:07 +0200

    Update to angular 15

--- dev/prepare-node.sh
+++ dev/prepare-node.sh
@@ -1,20 +1,20 @@
-#/bin/bash
+#!/bin/bash
 
 # Choose if you need to start console with proy or not
 # withProxy="--proxy-config proxy.conf.json"
 withProxy=""
 
 echo "Go to code directory..."
-cd /code
+cd /code || exit
 
 echo "Update npm..."
-npm install -g npm@8.5.0
+npm install -g npm@9.5.0
 
 echo "Install @angular/cli..."
-npm i -g @angular/cli@13
+npm i -g @angular/cli@15
 
 echo "Run 'npm i' (take some coffee...)"
 npm i
 
 echo "Run 'ng serve' for dev !!"
-ng serve --host 0.0.0.0 --disable-host-check ${withProxy}
+ng serve --host 0.0.0.0 --disable-host-check "${withProxy}"
